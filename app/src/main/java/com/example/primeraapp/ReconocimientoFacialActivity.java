411a71ba40249768b72278a5ab8c9404285b5695
Marcos_Acosta <macosta@icov.cl>
2023-07-11 20:10:02 -0400

    Asistencia RF, falta por realizar

---
+++ app/src/main/java/com/example/primeraapp/ReconocimientoFacialActivity.java
@@ -0,0 +1,20 @@
+package com.example.primeraapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.vision.CameraSource;
+
+public class ReconocimientoFacialActivity extends AppCompatActivity {
+
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reconocimiento_facial);
+    }
+}
