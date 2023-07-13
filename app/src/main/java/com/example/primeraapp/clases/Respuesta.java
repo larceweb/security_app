f06b732de0a9558c3234755e96dd09bcea69eb7b
Luciano Arce Sepulveda <larce@icov.cl>
2023-07-13 10:24:08 -0400

    se crean dos clases para recibir respuesta desde el login

---
+++ app/src/main/java/com/example/primeraapp/clases/Respuesta.java
@@ -0,0 +1,14 @@
+package com.example.primeraapp.clases;
+
+public class Respuesta {
+    private String message;
+    private String token;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getToken() {
+        return token;
+    }
+}
---
+++ app/src/main/java/com/example/primeraapp/utilidades/Constantes.java
@@ -0,0 +1,8 @@
+package com.example.primeraapp.utilidades;
+
+public class Constantes {
+    public static final String BASE_URL = "";
+    public static final String TOKEN = "token";
+    public static final String PHONE = "phone";
+    public static final String LOG_TAG = "node-login-android";
+}
