cdefa8828ae1be88b499b1b90c8f4be6a63bee92
Marcos_Acosta <macosta@icov.cl>
2023-07-16 17:39:41 -0400

    modal_pinerror

--- app/src/main/java/com/example/primeraapp/GenerarPinActivity.java
+++ app/src/main/java/com/example/primeraapp/GenerarPinActivity.java
@@ -12,11 +12,13 @@ import android.widget.Toast;
 
 import android.os.Bundle;
 
+import com.chaos.view.PinView;
+
 
 public class GenerarPinActivity extends AppCompatActivity {
 
-
-
+    public Button btnEnviar,btnVolver;
+PinView pinView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,5 +26,39 @@ public class GenerarPinActivity extends AppCompatActivity {
         setContentView(R.layout.activity_generar_pin);
 
 
+        pinView = findViewById(R.id.PinManual);
+
+        btnEnviar = findViewById(R.id.btnEnviar);
+        btnEnviar.setOnClickListener(v -> {
+            validarPin();
+        });
+
+        btnVolver = findViewById(R.id.btnvolver);
+        btnVolver.setOnClickListener(v -> {
+            startActivity(new Intent(GenerarPinActivity.this,AsistenciaActivity.class));
+            finish();
+        });
+
+
+
+
     }
+
+    public void validarPin(){
+
+        if(pinView.length()<=5){
+            pinView.setError("Complete el Pin");
+            Toast.makeText(this, "Verifica que ingresaste bien el PIN", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            Toast.makeText(this,"PIN ingresado",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+
+
+
+
+
 }
